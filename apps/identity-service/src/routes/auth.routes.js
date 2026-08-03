9478dbec363057df313e41d2b2e49e376b4d63e6
Furqan Ahmad <133624627+furqanahmadganaie@users.noreply.github.com>
2026-08-03 11:43:09 +0530

    feat(identity): add JWT authentication middleware and protected routes

--- apps/identity-service/src/routes/auth.routes.js
+++ apps/identity-service/src/routes/auth.routes.js
@@ -1,10 +1,18 @@
 // Defines the endpoint
 import express from "express";
 import { register,login } from "../controllers/auth.controller.js";
+import authenticate from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+
+router.get("/profile", authenticate, (req, res) => {
+  res.status(200).json({
+    success: true,
+    user: req.user,
+  });
+});
 
 export default router;
